afce300b92f9962bffc9b83e8872998798f42673
Peter Tworek <tworaz666@gmail.com>
2012-01-08 17:47:37 +0100

    Remove shell-aliases.

--- meta-jlime/recipes-core/images/jlime-base-image.bb
+++ meta-jlime/recipes-core/images/jlime-base-image.bb
@@ -1,7 +1,6 @@
 IMAGE_FEATURES += "package-management"
 
 POKY_EXTRA_INSTALL = "\
-    shell-aliases \
     file \
     htop \
     vim \
--- meta-jlime/recipes-extra/shell-aliases/files/aliases.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-alias ls="ls --color"
-alias ll="ls -lh"
-alias la="ls -A"
-alias lla="ls -lhA"
-
-alias f="find . | grep"
-alias c="clear"
